bb65d6698dfe3144a655b96968e65279a86ae7ba
sumit singh <sumitsingh.lko95@gmail.com>
2020-09-15 03:26:09 +0530

    receiving events

--- comments/index.js
+++ comments/index.js
@@ -35,6 +35,12 @@ app.post('/posts/:id/comments', async (req, res) => {
     res.status(200).send(comments);
 });
 
+app.post('/events', (req, res) => {
+    console.log('Event Received', req.body.type);
+
+    res.send({});
+});
+
 app.listen(4001, () => {
     console.log('Listening on port 4001');
 });
--- posts/index.js
+++ posts/index.js
@@ -29,6 +29,12 @@ app.post('/posts', async (req, res) => {
     res.status(201).send(posts[id]);
 });
 
+app.post('/events', (req, res) => {
+    console.log('Event Received', req.body.type);
+
+    res.send({});
+});
+
 app.listen(4000, () => {
     console.log('Listening on port 4000');
 });
