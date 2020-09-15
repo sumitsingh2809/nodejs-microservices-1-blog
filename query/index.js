7ff09302518a7260ad76ee93f57c70d740358e36
sumit singh <sumitsingh.lko95@gmail.com>
2020-09-15 16:15:20 +0530

    parsing incoming events

--- query/index.js
+++ query/index.js
@@ -6,8 +6,27 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-app.get('/posts', (req, res) => {});
+app.get('/posts', (req, res) => {
+    res.send(posts);
+});
 
-app.post('/events', (req, res) => {});
+app.post('/events', (req, res) => {
+    const { type, data } = req.body;
+
+    if (type === 'PostCreated') {
+        const { id, title } = data;
+        post[id] = { id, title, comments: [] };
+    }
+
+    if (type === 'CommentCreated') {
+        const { id, content, postId } = data;
+
+        const post = posts[postId];
+        post.comments.push({ id, content });
+    }
+
+    console.log(posts);
+    res.send({});
+});
 
 app.listen(4002, () => console.log('Listening on port 4002'));
