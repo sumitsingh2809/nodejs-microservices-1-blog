59a6bad6056c4ee5a5755ac44bb863079242e38b
sumit singh <sumitsingh.lko95@gmail.com>
2020-09-24 23:13:53 +0530

    updating comment content

--- comments/index.js
+++ comments/index.js
@@ -36,8 +36,23 @@ app.post('/posts/:id/comments', async (req, res) => {
     res.status(200).send(comments);
 });
 
-app.post('/events', (req, res) => {
+app.post('/events', async (req, res) => {
     console.log('Event Received', req.body.type);
+
+    const { type, data } = req.body;
+
+    if (type === 'CommentModerated') {
+        const { id, postId, status, content } = data;
+        const comments = commentsByPostId[postId];
+
+        const comment = comments.find((comment) => comment.id === id);
+        comment.status = status;
+
+        await axios.post('http://localhost:4005/events', {
+            type: 'CommentUpdated',
+            data: { id, postId, status, content },
+        });
+    }
 
     res.send({});
 });
