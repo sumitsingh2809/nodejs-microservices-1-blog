8a2242af07593612f762a389693caca3673fa0b7
sumit singh <sumitsingh.lko95@gmail.com>
2020-09-24 23:04:26 +0530

    Handling Moderation

--- event-bus/index.js
+++ event-bus/index.js
@@ -8,9 +8,10 @@ app.use(bodyParser.json());
 app.post('/events', (req, res) => {
     const event = req.body;
 
-    axios.post('http://localhost:4000/events', event);
-    axios.post('http://localhost:4001/events', event);
-    axios.post('http://localhost:4002/events', event);
+    axios.post('http://localhost:4000/events', event); //POSTS
+    axios.post('http://localhost:4001/events', event); //COMMENTS
+    axios.post('http://localhost:4002/events', event); //QUERY
+    axios.post('http://localhost:4003/events', event); //MODERATION
 
     res.send({ status: 'OK' });
 });
