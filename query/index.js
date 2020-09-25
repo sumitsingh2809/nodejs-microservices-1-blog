7bb4872e57f08509134a08319e2904a5b75f80b6
sumit singh <sumitsingh.lko95@gmail.com>
2020-09-25 19:33:37 +0530

    Implementing Event Sync

--- query/index.js
+++ query/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const axios = require('axios');
 
 const app = express();
 app.use(cors());
@@ -8,13 +9,7 @@ app.use(bodyParser.json());
 
 const posts = {};
 
-app.get('/posts', (req, res) => {
-    res.send(posts);
-});
-
-app.post('/events', (req, res) => {
-    const { type, data } = req.body;
-
+const handleEvent = (type, data) => {
     if (type === 'PostCreated') {
         const { id, title } = data;
         posts[id] = { id, title, comments: [] };
@@ -35,9 +30,27 @@ app.post('/events', (req, res) => {
         comment.status = status;
         comment.content = content;
     }
+};
 
+app.get('/posts', (req, res) => {
+    res.send(posts);
+});
+
+app.post('/events', (req, res) => {
+    const { type, data } = req.body;
+    handleEvent(type, data);
     console.log(posts);
     res.send({});
 });
 
-app.listen(4002, () => console.log('Listening on port 4002'));
+app.listen(4002, async () => {
+    console.log('Listening on port 4002 [QUERY]');
+
+    const res = await axios.get('http://localhost:4005/events');
+
+    for (const event of res.data) {
+        console.log('processing event:', event.type);
+
+        handleEvent(event.type, event.data);
+    }
+});
